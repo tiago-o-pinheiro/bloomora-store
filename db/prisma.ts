a6d8209946b6aac2f3d0623b2f3d8c0c30f73cb3
Tiago Pinheiro <tiagoliver@protonmail.com>
2025-09-17 15:50:48 +0200

    Fix: Revert DB values

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
-const DB_URL =
-  process.env.NODE_ENV === "development"
-    ? process.env.DATABASE_URL
-    : process.env.DATABASE_URL_PROD;
+// const DB_URL =
+//   process.env.NODE_ENV === "development"
+//     ? process.env.DATABASE_URL
+//     : process.env.DATABASE_URL_PROD;
 
-const adapter = new PrismaNeon({ connectionString: DB_URL! });
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
 export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     product: {
